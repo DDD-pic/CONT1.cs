e61179ab56f087a8808d3ea24a1b0050bf07f5c4
DDD-pic <sveta.moroz2010@yandex.ru>
2023-04-02 19:17:41 +0300

    zadacha

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿ string[] arraySimvol = {"BBB", "jjJ", "IIIIII"};//создаем  массив
+string[] GetArray (string[] array)
+// создаем метод сортирующий символы из массива. 
+
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        string simvol = array[i];
+        if (simvol.Length <= 3)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine();
+    Console.Write("Введенные символы длинной меньше либо равны 3 : ");
+    string[] arrayThreeSimvol = new string[count];
+    for (int i = 0, j = 0; i < array.Length; i++)
+    {
+        string simvol = array[i];
+        if (simvol.Length <= 3)
+        {
+            arrayThreeSimvol[j] = simvol;
+            j++;
+        }
+    }
+    return arrayThreeSimvol;
+}
+
+//печать массива
+void Print (string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine();
+}
+Print(GetArray(arraySimvol));
+Console.WriteLine();
